4106454cbe812d31481e2d69e166c33bc3fe944c
cordell <cordell.perry@gmail.com>
2018-02-06 15:46:53 -0500

    user connected/disconnected msg

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ res.sendFile(__dirname + '/index.html');
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
