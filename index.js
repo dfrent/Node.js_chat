bc3a7061fa1f58e24cd52a647213480978c0fede
cordell <cordell.perry@gmail.com>
2018-02-06 16:08:10 -0500

    emitting message events

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
