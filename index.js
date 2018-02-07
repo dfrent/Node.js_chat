6e3a8bb1488a5979b5e1eb2a6bf31dabb2dfec82
cordell <cordell.perry@gmail.com>
2018-02-07 18:11:47 -0500

    moved styling to own sheet

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+
+
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
-});
+}).use(express.static(path.join(__dirname, '/')));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log('user connected');
     io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
