61ac3974aa2d07477d9f7e60a19a1ff4f856c796
cordell <cordell.perry@gmail.com>
2018-02-08 23:45:24 -0500

    speech bubble styling

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ res.sendFile(__dirname + '/index.html');
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('user connected');
     io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
